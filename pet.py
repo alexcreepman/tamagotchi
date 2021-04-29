6f495fef388a15302b4069f7b997fd7987177241
alexcreepman <47357392+alexcreepman@users.noreply.github.com>
2021-04-29 03:36:02 +0300

    brushed up the code, added proper graphics, slightly reworked death mechanic

--- pet.py
+++ pet.py
@@ -2,74 +2,55 @@ from random import randrange
 
 
 class Pet:
-    chonkCap = 1200
+    chonk_cap = 1200
     # rewriting logic because of a bad variable name
     # sounds weird so here's a meme: https://imgur.com/sNqNi9E
-    joyCap = 1200
-    joyRestore = 10
-    chonkIncrease = 20
-    petPic = [list("      /\\\\ /\\\\                         ___  "),
-              list("   = o_o =_______          \\   \\ "),
-              list("    __^              __(     \\.__)  )"),
-              list("  <_____>__(_____)____/")]
-    eye = "o"
+    joy_cap = 1200
+    joy_restore = 10
+    chonk_increase = 20
 
-    def __init__(self, givenName):
-        self.name = givenName
-        self.chonkiness = randrange(2 * self.chonkCap // 3, self.chonkCap)
-        self.joy = randrange(self.joyCap // 2, self.joyCap)
+    def __init__(self, given_name):
+        self.name = given_name
+        self.chonkiness = randrange(self.chonk_cap // 3, self.chonk_cap)
+        self.joy = randrange(self.joy_cap // 3, self.joy_cap)
 
     def tick(self):
         if self.state() != "dead":
             self.chonkiness -= 3
             self.joy -= 2
+        else:
+            self.chonkiness = max(0, self.chonkiness)
+            self.joy = max(0, self.joy)
 
     def state(self):
         if self.chonkiness <= 0 or self.joy <= 0:
             return "dead"
-        if self.chonkiness <= self.chonkCap // 3:
+        if self.chonkiness <= self.chonk_cap // 3:
             return "hungry"
-        if self.joy <= self.joyCap // 3:
+        if self.joy <= self.joy_cap // 3:
             return "bored"
         return "happy"
 
-    def reduceChonk(self, val=chonkIncrease):
-        self.chonkiness = min(self.chonkCap,
+    def reduce_chonk(self, val=chonk_increase):
+        self.chonkiness = min(self.chonk_cap,
                               self.chonkiness + randrange(val // 2, val))
 
-    def reduceBoredom(self, val=joyRestore):
-        self.joy = min(self.joyCap, self.joy + randrange(val // 2, val))
+    def reduce_boredom(self, val=joy_restore):
+        self.joy = min(self.joy_cap, self.joy + randrange(val // 2, val))
 
     def feed(self):
         if self.state() == "dead":
             return
-        self.reduceChonk()
-        # print(self.name + " is fed!")
+        self.reduce_chonk()
 
     def play(self):
         if self.state() == "dead":
             return
-        self.reduceBoredom()
-        # print(self.name + " is happy to play!")
+        self.reduce_boredom()
 
-    def getStatus(self):
+    def get_status(self):
         st = self.state()
-        if st == "dead":
-            eye = "x"
-        elif st == "hungry":
-            eye = "o"
-        elif st == "bored":
-            eye = "-"
-        elif st == "happy":
-            eye = "^"
-        else:
-            eye = "?"
+        return "graphics/" + st + ".png"
 
-        self.petPic[1][5] = eye
-        self.petPic[1][7] = eye
-        status = [line.copy() for line in self.petPic]
-        status[1] += " Current joy:        " + str(self.joy)
-        status[2] += " Current chonkiness: " + str(self.chonkiness)
-        status[3] += " " + self.name + " is " + self.state() + "!"
-        lines = [''.join(line) for line in status]
-        return lines
+    def is_dead(self):
+        return " is dead :(" if self.state() == "dead" else ""
